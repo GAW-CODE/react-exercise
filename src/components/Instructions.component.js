e07ecd53e8be301874435066c3175e377aaddc4d
Allen Zhang <1124796703@qq.com>
2020-11-13 23:49:15 -0800

    Part 1 Solution

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react'
 import './../styles/instructions.css'
 
 class Instructions extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <div className="instructions">
         Follow the instructions on the README to get started!
-        <img
-          className="instructions__logo"
-          src="https://i.imgur.com/0bXP0MU.png"
-          alt="Nadeshiko Kawaii"
-        />
+        {this.props.shouldDisplayImage && (
+          <img
+            className="instructions__logo"
+            src="https://i.imgur.com/0bXP0MU.png"
+            alt="Nadeshiko Kawaii"
+          />
+        )}
       </div>
     )
   }
